97ba1a04f7b88f81ed3a02f07922224dbee6b156
choihana <chhana0703@gmail.com>
2023-08-01 00:46:38 +0900

    feat:chat with sqs

---
+++ server1/src/main/java/com/example/server1/ChatController.java
@@ -0,0 +1,35 @@
+package com.example.server1;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+
+	private final ChatRepository chatRepository;
+	private final SqsChatSender sqsChatSender;
+
+
+	// 채팅 메시지 생성해서 AWS SQS로 전송
+	// @CrossOrigin // 귓속말 할때 사용
+	@PostMapping("")
+	public Mono<Chat> sendMessage(@RequestBody Chat chat) {
+		// chat.setCreatedAt(LocalDateTime.now());
+		// Chat chat = new Chat(chatRequestDto);
+		sqsChatSender.sendMessage(chat);
+		return chatRepository.save(chat);
+		//
+		// return new ResponseEntity<>(new ResponseDto(200, "전송됨",chat), HttpStatus.OK);
+	}
+
+
+
+}
