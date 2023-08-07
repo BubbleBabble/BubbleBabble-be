d9c24869d35ed0d4ce3bbf850ccd8c1fa754aef3
choihana <chhana0703@gmail.com>
2023-08-07 22:39:02 +0900

    fix:change unicast

--- server2/src/main/java/com/example/server2/ChatService.java
+++ server2/src/main/java/com/example/server2/ChatService.java
@@ -1,19 +1,23 @@
 package com.example.server2;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Sinks;
+import reactor.util.concurrent.Queues;
 
 @Service
 public class ChatService {
-	private final Sinks.Many<Chat> sink = Sinks.many().multicast().onBackpressureBuffer();
+	private final Sinks.Many<Chat> sink = Sinks.many().unicast().onBackpressureBuffer();
 
+	@Transactional
 	public void getChat(Chat chat){
 		sink.tryEmitNext(chat);
 	}
 
 	public Flux<Chat> getChatFlux(){
+
 		return sink.asFlux();
 	}
 }
